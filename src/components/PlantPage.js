e8c37e3515d7824eefe4fa25d00f432e30354d1b
+ <+>
2025-01-13 18:32:17 -0600

    website works, tests are not passing

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+const [plants, setPlants] = useState([]);
+const [searchTerm, setSearchTerm] = useState('');
+
+useEffect(() => {
+  fetch ('http://localhost:6001/plants')
+  .then (res => res.json())
+  .then (data => setPlants(data))
+}, []);
+
+function addPlant(object) {
+  setPlants(plants => [...plants, object])
+}
+
+function searchPlants (string) {
+  setSearchTerm(string)
+}
+
+const itemsToDisplay = plants.filter((plant) => 
+  plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+)
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addPlant={addPlant}/>
+      <Search onSearch={searchPlants}/>
+      <PlantList plants={itemsToDisplay}/>
     </main>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,20 @@
 import React from "react";
 
-function Search() {
+function Search( {onSearch} ) {
+
+  function findPlant(e) {
+    onSearch(e.target.value);
+  }
+
   return (
     <div className="searchbar">
       <label htmlFor="search">Search Plants:</label>
       <input
+        onChange={findPlant}
         type="text"
         id="search"
         placeholder="Type a name to search..."
-        onChange={(e) => console.log("Searching...")}
+       
       />
     </div>
   );
